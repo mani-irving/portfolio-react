e4b9e62b7233acd3292fa0c7c75c3e09e18f7a34
Manikant Pandey <mani73244@gmail.com>
2025-05-16 09:32:15 +0530

    added animations in project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { motion } from "motion/react";
 import ProjectCard from "../card component/ProjectCard";
 import ProjectDetails from "../user details/ProjectDetails";
 
 export default function Project() {
   return (
-    <section
+    <motion.section
       id="Projects"
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 2 }}
       className="bg-white text-black dark:bg-gray-900 dark:text-white pt-20 px-6 md:px-12 min-h-[100vh]"
     >
       <div className="text-center mb-10">
@@ -26,6 +31,6 @@ export default function Project() {
           />
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 }
